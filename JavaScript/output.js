5bda5d08db55a43c6185c6120ad48472ffe364be
yagami light <halysl0817@gmail.com>
2019-02-13 16:59:08 +0800

    add js-四种输出方式

--- JavaScript/output.js
+++ JavaScript/output.js
@@ -3,9 +3,15 @@ function window_alert(){
 }
 
 function write(){
-    document.write(5+6);
+    document.write(Date());
 }
 
 function operate_html(id_name){
     document.getElementById(id_name).innerHTML = "我被修改了啊！";
+}
+
+function write_on_console(){
+    a = 5;
+    b = 6;
+    console.log("a =", a, "b=", b, "c=", a+b);
 }
